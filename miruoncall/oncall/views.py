b2bd5dec5fa8b84a9832ebc4b525ba326437ad2d
Damian Myerscough <Damian.Myerscough@gmail.com>
2019-01-09 00:23:22 -0800

    Parse users datetime

--- miruoncall/oncall/views.py
+++ miruoncall/oncall/views.py
@@ -2,6 +2,8 @@
 
 from datetime import timedelta
 
+import dateutil.parser
+import pytz
 from django.core.cache import cache
 from django.http import JsonResponse
 from django.utils import timezone
@@ -10,6 +12,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from oncall.models import Incidents
+from oncall.serializer import IncidentSerializer
 
 
 class Oncall(APIView):
@@ -18,20 +21,27 @@ class Oncall(APIView):
 
     def get(self, request):
         """
+        List all incidents
+
+        TODO: Check the since date is not greater than the until date
 
         :param request:
 
         :return:
         """
         try:
-            since = request.data.get('since', (timezone.now() - timedelta(days=7)).strftime('%Y-%m-%d'))
-            until = request.data.get('until', timezone.now().strftime('%Y-%m-%d'))
+            since = dateutil.parser.parse(request.data.get('since', (timezone.now() - timedelta(days=7)).isoformat()))
+            until = dateutil.parser.parse(request.data.get('until', timezone.now().isoformat()))
         except ValueError:
-            return JsonResponse({'error': 'Invalid date format YYYY/MM/DD'}, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse({'error': 'Invalid date format YYYY-MM-DD'}, status=status.HTTP_400_BAD_REQUEST)
 
-        incidents = Incidents.objects.filter(created_at__range=[since, until])
+        incidents = Incidents.objects.filter(
+            created_at__range=[since.replace(tzinfo=pytz.UTC), until.replace(tzinfo=pytz.UTC)]
+        )
 
-        return JsonResponse({'incidents': 1}, status=status.HTTP_200_OK)
+        return JsonResponse(
+            {'incidents': IncidentSerializer(incidents, many=True).data}, status=status.HTTP_200_OK
+        )
 
     def post(self, request):
         pass
