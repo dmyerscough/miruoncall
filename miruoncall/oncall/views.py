a075cc6f14353c444cad5071b4861dc47e876306
Damian Myerscough <Damian.Myerscough@gmail.com>
2019-01-09 23:54:11 -0800

    List incidents and the number of incidents that occured on a specific date

--- miruoncall/oncall/views.py
+++ miruoncall/oncall/views.py
@@ -22,25 +22,31 @@ class Oncall(APIView):
     def get(self, request):
         """
         List all incidents
-
-        TODO: Check the since date is not greater than the until date
-
-        :param request:
-
-        :return:
         """
+        incident_count_by_day = {}
+
         try:
             since = dateutil.parser.parse(request.data.get('since', (timezone.now() - timedelta(days=7)).isoformat()))
             until = dateutil.parser.parse(request.data.get('until', timezone.now().isoformat()))
         except ValueError:
             return JsonResponse({'error': 'Invalid date format YYYY-MM-DD'}, status=status.HTTP_400_BAD_REQUEST)
 
+        if since > until:
+            return JsonResponse({'error': 'Since cannot be newer than until'}, status=status.HTTP_400_BAD_REQUEST)
+
         incidents = Incidents.objects.filter(
             created_at__range=[since.replace(tzinfo=pytz.UTC), until.replace(tzinfo=pytz.UTC)]
-        )
+        ).order_by('-created_at')
+
+        for incident in incidents:
+            incident_count_by_day.setdefault(incident.created_at.strftime('%Y-%m-%d'), 0)
+            incident_count_by_day[incident.created_at.strftime('%Y-%m-%d')] += 1
 
         return JsonResponse(
-            {'incidents': IncidentSerializer(incidents, many=True).data}, status=status.HTTP_200_OK
+            {
+                'incidents': IncidentSerializer(incidents, many=True).data,
+                'incident_count': incident_count_by_day,
+            }, status=status.HTTP_200_OK
         )
 
     def post(self, request):
